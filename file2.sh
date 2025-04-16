fab2f56508837fa9e785309b7400d998fe8a4f18
etech-tsona <gaetan2026@gmail.com>
2025-04-16 16:19:14 +0100

    file2 removed

--- file2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "This machine was powered by $(logname) and it has been running for `uptime`"
-echo "This machine was powered by $LOGNAME and it has been running for $(uptime)"
-echo "just to demo"
-
